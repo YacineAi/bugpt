414c296ebcf3ad2241a58feb1e5b1a2fa60df266
Yacine Djenidi <djenidi.dev@gmail.com>
2023-08-05 02:53:12 +0100

    Add : API

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(process.env.SB_URL, process.env.SB_KEY, { auth: { persistSession: false} });
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-// Load data from JSON file
-const dataPath = './data.json';
+/* ----- DB Qrs ----- */
+async function createUser(user) {
+  const { data, error } = await supabase
+      .from('users')
+      .insert([ user ]);
+
+    if (error) {
+      throw new Error('Error creating user : ', error);
+    } else {
+      return data
+    }
+};
+
+async function updateUser(id, update) {
+  const { data, error } = await supabase
+    .from('users')
+    .update( update )
+    .eq('uid', id);
+
+    if (error) {
+      throw new Error('Error updating user : ', error);
+    } else {
+      return data
+    }
+};
+
+async function userDb(type) {
+  const { data, error } = await supabase
+    .from('tokens')
+    .select('*')
+    .eq('type', type);
+
+  if (error) {
+    console.error('Error checking user:', error);
+  } else {
+    return data
+  }
+};
+const getoken = async () => {
+  const tokens = await userDb("openai");
+  var random = Math.floor(Math.random() * tokens.length);
+  return tokens[random].token
+}
+
+const headers = {
+  'accept-encoding': 'gzip',
+  'authorization': `Bearer ${getoken()}`,
+  'connection': 'Keep-Alive',
+  'content-type': 'application/json; charset=UTF-8',
+  'host': 'api.openai.com',
+  'user-agent': 'okhttp/4.10.0'
+};
 
 app.get('/', (req, res) => {
   const tableData = loadTableData();
@@ -21,7 +72,7 @@ app.post('/add', (req, res) => {
   const tableData = loadTableData();
   tableData.push(newRowData);
   saveTableData(tableData);
-  res.redirect('/');
+  res.json({ success: true, newRowData });
 });
 
 app.post('/remove/:index', (req, res) => {
@@ -30,11 +81,17 @@ app.post('/remove/:index', (req, res) => {
   if (index >= 0 && index < tableData.length) {
     tableData.splice(index, 1);
     saveTableData(tableData);
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, message: 'Invalid index' });
   }
-  res.redirect('/');
 });
 
-// Helper functions to read and write data to JSON file
+app.post('/openai/chat', async (req, res) => {
+  const response = await axios.post('https://api.openai.com/v1/chat/completions', req.body, { headers });
+  res.json(response.data);
+});
+
 function loadTableData() {
   try {
     const data = fs.readFileSync(dataPath);
@@ -53,6 +110,6 @@ function saveTableData(tableData) {
   }
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on 3000`);
 });
